c161373cb2546a3149274583176dc535f856a3ff
Max Hammerlund <hammerlundm@gmail.com>
2020-02-01 13:18:20 -0600

    Basic item management

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,25 +3,21 @@ extends KinematicBody
 export(float) var MOVE_SPEED = 1000
 export(float) var TURN_SPEED = 0.001
 export(float) var FRICTION = 0.01
+export(float) var INTERACT_SIZE = 500
 
 var v = Vector3(0, 0, 0)
 var angle = Vector2(0, 0)
+var old_pos = Vector2(0, 0)
+
+var object = null
 
 func _ready():
-	pass
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	$reach.connect("area_entered", self, "select")
+	$reach.connect("area_exited", self, "deselect")
 
 func _process(delta):
-	"""if Input.is_action_pressed("turn_left"):
-		rotate_y(TURN_SPEED * delta)
-		facing = facing.rotated(Vector3(0, 1, 0), TURN_SPEED * delta)
-	if Input.is_action_pressed("turn_right"):
-		rotate_y(-TURN_SPEED * delta)
-		facing = facing.rotated(Vector3(0, 1, 0), -TURN_SPEED * delta)"""
 	transform.basis = Basis()
-	var size = get_viewport().size
-	var mouse = get_viewport().get_mouse_position()
-	var asdf = mouse - (size * 0.5)
-	angle += asdf * TURN_SPEED * delta
 	rotate(transform.basis.y, -angle.x)
 	rotate(transform.basis.x, -angle.y)
 	if Input.is_action_pressed("move_forward"):
@@ -32,5 +28,35 @@ func _process(delta):
 		v -= transform.basis.x * delta * MOVE_SPEED
 	if Input.is_action_pressed("move_right"):
 		v += transform.basis.x * delta * MOVE_SPEED
-	v *= FRICTION / delta
+	#v.y -= 5
+	if Input.is_action_pressed("interact"):
+		if object == null:
+			var objects = get_tree().get_nodes_in_group("object")
+			for obj in objects:
+				if obj.selected:
+					print("!!!")
+					object = obj
+					obj.get_parent().remove_child(obj)
+					add_child(obj)
+		else:
+			var heart = get_tree().get_nodes_in_group("heart")
+			for part in heart:
+				if part.selected:
+					if part.place(object):
+						object = null
+	v *= FRICTION / (delta + 0.01)
 	move_and_slide(v)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		angle += event.relative * TURN_SPEED
+
+func select(area):
+	var groups = area.get_parent().get_groups()
+	if groups.has("object") or groups.has("heart"):
+		area.get_parent().selected = true
+
+func deselect(area):
+	var groups = area.get_parent().get_groups()
+	if groups.has("object") or groups.has("heart"):
+		area.get_parent().selected = false
