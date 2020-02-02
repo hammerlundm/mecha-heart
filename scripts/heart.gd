8c997e2691686d538e129f25e7939c1ab0695574
Max Hammerlund <hammerlundm@gmail.com>
2020-02-02 08:47:07 -0600

    delete lights

--- scripts/heart.gd
+++ scripts/heart.gd
@@ -1,14 +1,15 @@
 extends Spatial
 
 export(String) var part_name
+export(PackedScene) var part_scene
 
 var selected
 
 func place(obj):
 	if obj.part_name == part_name:
-		print(":)")
 		obj.queue_free()
+		var inst = part_scene.instance()
+		$"..".add_child(inst)
 		return true
 	else:
-		print(":(")
 		return false
--- scripts/object.gd
+++ scripts/object.gd
@@ -1,5 +1,11 @@
 extends Spatial
 
 export(String) var part_name
+export(Mesh) var mesh
+export(Material) var mat
 
 var selected
+
+func _ready():
+	$body/shape.mesh = mesh
+	$body/shape.material_override = mat
